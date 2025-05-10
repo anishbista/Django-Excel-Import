e250cef343ffe1639dc54206ac521d5eed2d1cb7
anishbista <anishbista9235@gmail.com>
2025-05-10 23:10:35 +0545

    changes in settings

--- django_excel_import/wsgi.py
+++ django_excel_import/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_excel_import.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_excel_import.settings")
+django.setup()
+
+# Run migrations automatically
+from django.core.management import call_command
+
+call_command("migrate", interactive=False)
 
 application = get_wsgi_application()
